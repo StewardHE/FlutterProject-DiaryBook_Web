624eeee341d040e2b358a7a3067c30de5024de01
Steward <bryanherreraestrada@gmail.com>
2022-09-26 19:23:59 -0600

    I made changes to the code, i made that the page looks empty and ready to start the DiaryBook

--- diary_book/lib/main.dart
+++ diary_book/lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  // this widget is the root of the application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyDiary(),
+    );
+  }
+}
+
+class MyDiary extends StatelessWidget {
+  const MyDiary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
