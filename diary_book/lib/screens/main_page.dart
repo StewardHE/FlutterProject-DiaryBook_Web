04bcbaf44e6a3c790077242d07353bb6f18ed8d3
Steward <bryanherreraestrada@gmail.com>
2022-10-22 15:41:53 -0600

    I divided the body of the webApp into 2 pieces

--- diary_book/lib/screens/main_page.dart
+++ diary_book/lib/screens/main_page.dart
@@ -13,103 +13,128 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     // this will make the navbar
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.grey.shade100,
-      toolbarHeight: 100,
-      elevation: 4, // this add the shadown to the navbar
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
+        toolbarHeight: 100,
+        elevation: 4, // this add the shadown to the navbar
 
-      // this add the logo DiaryBook in the navbar
-      title: Row(children: [
-        Text(
-          'Diary',
-          style: TextStyle(fontSize: 39, color: Colors.blueGrey.shade400),
-        ),
-        Text(
-          'Book',
-          style: TextStyle(fontSize: 39, color: Colors.green),
-        )
-      ]),
+        // this add the logo DiaryBook in the navbar
+        title: Row(children: [
+          Text(
+            'Diary',
+            style: TextStyle(fontSize: 39, color: Colors.blueGrey.shade400),
+          ),
+          Text(
+            'Book',
+            style: TextStyle(fontSize: 39, color: Colors.green),
+          )
+        ]),
 
-      // dropdown menu
+        // dropdown menu
 
-      actions: [
-        Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: DropdownButton<String>(
-                items: <String>['Latest', 'Earliest'].map((String value) {
-                  return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(
-                        value, // this is the value of the selecter, for defect it is Empty
-                        style: TextStyle(color: Colors.grey),
-                      ));
-                }).toList(),
+        actions: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButton<String>(
+                  items: <String>['Latest', 'Earliest'].map((String value) {
+                    return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value, // this is the value of the selecter, for defect it is Empty
+                          style: TextStyle(color: Colors.grey),
+                        ));
+                  }).toList(),
 
-                // this make that if the dropdownText is null or empty,
-                // Show 'Select' in the bottom of the page
-                // But if already selected Latest of earliest, show them
-                // Or
-                //If no option has been selected in the button,
-                // 'Select' will appear, but if there is already an option selected,
-                // the selected option will appear
-                hint: (_dropDownText == null)
-                    ? Text('Select')
-                    : Text(_dropDownText!),
-                onChanged: (value) {
-                  // check the state
-                  if (value == 'Latest') {
-                    setState(() {
-                      _dropDownText = value;
-                    });
-                  } else if (value == 'Earliest') {
-                    setState(() {
-                      _dropDownText = value;
-                    });
-                  }
-                },
+                  // this make that if the dropdownText is null or empty,
+                  // Show 'Select' in the bottom of the page
+                  // But if already selected Latest of earliest, show them
+                  // Or
+                  //If no option has been selected in the button,
+                  // 'Select' will appear, but if there is already an option selected,
+                  // the selected option will appear
+                  hint: (_dropDownText == null)
+                      ? Text('Select')
+                      : Text(_dropDownText!),
+                  onChanged: (value) {
+                    // check the state
+                    if (value == 'Latest') {
+                      setState(() {
+                        _dropDownText = value;
+                      });
+                    } else if (value == 'Earliest') {
+                      setState(() {
+                        _dropDownText = value;
+                      });
+                    }
+                  },
+                ),
               ),
-            ),
 
-            // This will make the avatar Circle
-            Container(
-              child: Row(
-                children: [
-                  Column(
-                    children: [
-                      Expanded(
-                          child: InkWell(
-                        child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: CircleAvatar(
-                              radius: 30,
-                              backgroundImage: NetworkImage(
-                                  'https://picsum.photos/id/237/200/300'),
-                              backgroundColor: Colors.transparent,
-                            )),
-                      )),
-                      // This add the name under the user photo
-                      Text(
-                        'James',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                  // this will add the Logout Button
-                  IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.logout_outlined,
-                        size: 19,
-                        color: Colors.red,
-                      ))
-                ],
-              ),
-            )
-          ],
-        ),
-      ],
-    ));
+              // This will make the avatar section
+              Container(
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Expanded(
+                            child: InkWell(
+                          child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: CircleAvatar(
+                                radius: 30,
+                                backgroundImage: NetworkImage(
+                                    'https://picsum.photos/id/237/200/300'),
+                                backgroundColor: Colors.transparent,
+                              )),
+                        )),
+                        // This add the name under the user photo
+                        Text(
+                          'James',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    // this will add the Logout Button
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.logout_outlined,
+                          size: 19,
+                          color: Colors.red,
+                        ))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+
+      // The body Of main Page
+      body: Row(
+        children: [
+          // this divide the body in two parts
+          Expanded(
+              flex: 4,
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    border: Border(
+                        right: BorderSide(width: 0.4, color: Colors.blueGrey))),
+                // color: Colors.green,
+                child: Column(),
+              )),
+          Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.white,
+                child: Column(),
+              ))
+        ],
+      ),
+    );
   }
 }
